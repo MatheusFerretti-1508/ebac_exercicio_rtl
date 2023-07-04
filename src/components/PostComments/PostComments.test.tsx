7eb925f8e2bf832bbdd0e0101bc95e6247ba445e
Matheus Ferretti <matheus10.ferretti@gmail.com>
2023-07-04 09:23:35 -0300

    Concluindo o exercicio

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,29 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    it('Espera-se que o componente renderize', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    it('Para adicionar comentários', () => {
+        render(<PostComment/>);
+
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: {
+                value: 'Comentário com teste, um',
+            }
+        });
+        fireEvent.click(screen.getByTestId('comment-button'));
+    
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: {
+                value: 'Comentário com teste, dois',
+            }
+        });
+        fireEvent.click(screen.getByTestId('comment-button'));
+
+        expect(screen.getAllByTestId('comment-element')).toHaveLength(2);
     });
 });
